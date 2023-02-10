481a2b5f6ca1347f1d1a278a8a8b8b0538059493
nikita <saltykovnikson@gmail.com>
2023-02-10 15:00:26 +0300

    feat: add stats-server functional

---
+++ stat_svc/stat_svc_server/src/main/java/ru/practicum/server/model/ViewStats.java
@@ -0,0 +1,16 @@
+package ru.practicum.server.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ViewStats {
+
+    private String app;
+    private String uri;
+    private long hits;
+
+}
