f906e516a19db8aaf9ddb8562a6cb26cdf9e1208
nikita <saltykovnikson@gmail.com>
2023-03-12 18:44:09 +0300

    feat: made functionality of main-server.

---
+++ main_svc/src/main/java/ru/practicum/main/dto/UpdateEventUserDto.java
@@ -0,0 +1,34 @@
+package ru.practicum.main.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import ru.practicum.main.enums.StateActionForUser;
+import ru.practicum.main.models.Location;
+
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class UpdateEventUserDto {
+    @Size(min = 20, max = 2000)
+    private String annotation;
+    private Long category;
+    @Size(min = 20, max = 7000)
+    private String description;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime eventDate;
+    private Location location;
+    private Boolean paid;
+    private Long participantLimit;
+    private Boolean requestModeration;
+    private StateActionForUser stateAction;
+    @Size(min = 2, max = 120)
+    private String title;
+}
+
